492f795f80111ad3f9b7021b670da476877b4386
vanshavenger <vanshchopra101@gmail.com>
2024-11-22 21:23:38 +0530

    Will contract

---
+++ will/will.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Will {
+    address private owner;  
+    address private recipient;
+    uint256 private lastPingTimestamp;
+
+    constructor(address _receipt) {
+        owner = msg.sender;
+        recipient = _receipt;
+        lastPingTimestamp = block.timestamp;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier onlyRecipient {
+        require(msg.sender == recipient, "Not recipient");
+        _;
+    }
+
+    function changeRecipient(address _newRecipient) public onlyOwner {
+        recipient = _newRecipient;
+    }
+
+    function ping() public onlyOwner() returns (address, address) {
+        lastPingTimestamp = block.timestamp;
+        return (owner, recipient);
+    }
+
+    function drain() public onlyRecipient {
+        require(block.timestamp > lastPingTimestamp + 365 days, "Owner has pinged recently");
+        
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to drain");
+        
+        (bool sent, ) = recipient.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    receive() external payable {}
+}
