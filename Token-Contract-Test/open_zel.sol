852a772208df12a4da34fa38d2a8483a7d143ec5
vanshavenger <vanshchopra101@gmail.com>
2024-11-29 21:41:30 +0530

    ERC20

---
+++ Token-Contract-Test/open_zel.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VanshToken is ERC20, Ownable {
+
+    constructor() ERC20("VanshToken", "VT") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000000);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+}
