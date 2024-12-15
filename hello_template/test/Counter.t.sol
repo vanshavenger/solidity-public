54fb312a93a2d0a6c5da1c4a26e9353bb7eb3440
vanshavenger <vanshchopra101@gmail.com>
2024-12-15 17:51:27 +0530

    fail test

--- hello_template/test/Counter.t.sol
+++ hello_template/test/Counter.t.sol
@@ -9,41 +9,70 @@ contract TestCounter is Test {
     Counter c;
 
     function setUp() public {
-        c = new Counter(100);
+        c = new Counter(1);
+    }
+
+    function testNum() public {
+        assertEq(c.num(), 1, "Counter should start at 1");
     }
 
     function testIncrement() public {
         c.increment();
-        assertEq(c.num(), 101);
+        assertEq(c.num(), 2, "Counter should increment by 1");
     }
 
     function testDecrement() public {
         c.decrement();
-        assertEq(c.num(), 99);
+        assertEq(c.num(), 0);
     }
 
     function testIncrementTwice() public {
         c.increment();
         c.increment();
-        assertEq(c.num(), 102);
+        assertEq(c.num(), 3);
     }
 
-    function testDecrementTwice() public {
+    function testFailDecrementTwice() public {
         c.decrement();
-        c.decrement();
-        assertEq(c.num(), 98);
+        
+        c.decrement();c.decrement();c.decrement();c.decrement();c.decrement();
     }
 
-    function testIncrementDecrement() public {
-        c.increment();
-        c.decrement();
-        assertEq(c.num(), 100);
-    }
 
-    function testDecrementIncrement() public {
-        c.decrement();
-        c.increment();
-        assertEq(c.num(), 100);
-    }
+
+    // function testIncrement() public {
+    //     c.increment();
+    //     assertEq(c.num(), 101, "Counter should increment by 1");
+    // }
+
+    // function testDecrement() public {
+    //     c.decrement();
+    //     assertEq(c.num(), 99);
+    // }
+
+    // function testIncrementTwice() public {
+    //     c.increment();
+    //     c.increment();
+    //     assertEq(c.num(), 102);
+    // }
+
+    // function testDecrementTwice() public {
+    //     c.decrement();
+    //     c.decrement();
+    //     assertEq(c.num(), 98);
+    // }
+
+    // function testIncrementDecrement() public {
+    //     c.increment();
+    //     c.decrement();
+    //     assertEq(c.num(), 100);
+    // }
+
+    // function testDecrementIncrement() public {
+    //     c.decrement();
+    //     c.increment();
+    //     assertEq(c.num(), 100);
+    // }
+    
 
 }
