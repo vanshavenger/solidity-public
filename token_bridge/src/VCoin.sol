ac3ec53993be43c0d9a2e2323ada49f01d76f6b3
vanshavenger <vanshchopra101@gmail.com>
2024-12-20 20:34:55 +0530

    brige init

--- token_bridge/src/VCoin.sol
+++ token_bridge/src/VCoin.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract VCoin { }
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VCoin is ERC20 { }
